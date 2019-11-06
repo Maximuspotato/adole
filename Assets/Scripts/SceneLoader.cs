af4ab34d7e45df7125c90097a41a656ecedb207c
Maximuspotato <rickylui28@gmail.com>
2019-11-06 23:41:26 +0300

    1st commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadMainScene(){
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+}
